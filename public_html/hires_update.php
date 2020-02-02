d505e894986d9aac635774e125f27dfc60e2c6e5
Homerocker <molodoy3561@gmail.com>
2020-02-03 01:38:47 +0300

    initial commit

--- public_html/hires_update.php
+++ public_html/hires_update.php
@@ -1,6 +1,9 @@
 <?php
 
 require $_SERVER['DOCUMENT_ROOT'] . '/../config.php';
+if ($_SESSION['user_id'] != 1) {
+    exit;
+}
 $sql = $db->query('select id, icon from game_content_armours');
 $i = $j = 0;
 while ($row = $sql->fetch_assoc()) {
